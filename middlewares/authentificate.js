ebd0370a82ae5bad79116e0c54756f42514fed5c
SerhiiSamara <ndaair@gmail.com>
2023-04-01 00:25:28 +0300

    fix update

--- middlewares/authentificate.js
+++ middlewares/authentificate.js
@@ -7,12 +7,15 @@ const { SECRET_KEY } = process.env;
 const authentificate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
+  console.log(bearer);
+  console.log(token);
   if (bearer !== "Bearer") {
     next(HttpError(401));
   }
   try {
-    const { id } = jwt.verify(token, SECRET_KEY);
-    const user = await User.findById(id);
+    const { _id } = jwt.verify(token, SECRET_KEY);
+    console.log(_id);
+    const user = await User.findById(_id);
     if (!user || !user.token || user.token !== token) {
       next(HttpError(401));
     }
