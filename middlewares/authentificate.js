5f264979aa04f8fa431ec9970488803e88d17a54
Tanets Olexandr <104691488+AlexTanetsDev@users.noreply.github.com>
2023-04-01 00:48:10 +0300

    delete console logs

--- middlewares/authentificate.js
+++ middlewares/authentificate.js
@@ -7,14 +7,13 @@ const { SECRET_KEY } = process.env;
 const authentificate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  console.log(bearer);
-  console.log(token);
+  
   if (bearer !== "Bearer") {
     next(HttpError(401));
   }
   try {
     const { _id } = jwt.verify(token, SECRET_KEY);
-    console.log(_id);
+   
     const user = await User.findById(_id);
     if (!user || !user.token || user.token !== token) {
       next(HttpError(401));
