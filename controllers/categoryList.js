126fcb6b16e33c6ca168efa572191de1a199eec7
ChemerysIgor <chemeris_igor@ukr.net>
2023-04-01 19:47:48 +0300

    update

--- controllers/categoryList.js
+++ controllers/categoryList.js
@@ -1,4 +1,4 @@
-const category = require("../models/category");
+const category = require("../models/category/index");
 
 const getCategoryList = async (req, res) => {
   const result = await category.getCategoryList();
