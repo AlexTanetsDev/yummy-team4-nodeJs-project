623e3ca2b7be558428d3858724de7b34afcf4ac4
ChemerysIgor <chemeris_igor@ukr.net>
2023-04-02 18:11:51 +0300

    Updete ingredientList

--- controllers/ingredientList.js
+++ controllers/ingredientList.js
@@ -3,7 +3,6 @@ const { Ingredient } = require("../models/ingredient");
 const { controllersWrapper, HttpError } = require("../helpers");
 
 const getAllIngredients = async (req, res) => {
-  console.log("функція працює");
   const result = await Ingredient.find();
 
   if (!result) {
