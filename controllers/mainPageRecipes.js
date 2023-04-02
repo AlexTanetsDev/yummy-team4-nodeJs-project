62ee162016bf0cb26ca86568edbc6f7df97ed762
TechnicUnion <103375230+TechnicUnion@users.noreply.github.com>
2023-04-03 01:58:42 +0300

    fixed main-page

--- controllers/mainPageRecipes.js
+++ controllers/mainPageRecipes.js
@@ -1,27 +1,33 @@
 const { Recipe } = require("../models/recipe");
-
 const { controllersWrapper, HttpError } = require("../helpers");
 
+const fs = require("fs/promises");
+const path = require("path");
+const categoryListPath = path.join(
+  __dirname,
+  "..",
+  "StaticData",
+  "categoryList.json"
+);
+
 const getMainPageRecipes = async (req, res) => {
-  
-    // const { category } = req.query;
-  
-    const category = ["Miscellaneous", "Breakfast", "Vegan", "Dessert"]
+
+  const categoryList = await fs.readFile(categoryListPath, "utf-8");
+  const category = JSON.parse(categoryList);
 
     // const { page = 1, limit = 10 } = req.query;
     // const skip = (page - 1) * limit;
-
-    // const result = await Recipe.find({ category: category }, "-createdAT -updateAt", {skip, limit});
-    
-  const result = await Recipe.find({ category: category } ) ;
   
+  const recipesList = await Recipe.find({ category: category }, "_id title category preview author");
   
-    if (!result) {
+    if (!recipesList) {
       throw HttpError(404, 'Not found');
     }
   
-  result.sort((a, b) => a.category.localeCompare(b.category));
-  
+  const result = category.map((item) => {
+    return {category: item, recipec: recipesList.filter(dish => dish.category === item).slice(0, 4)}
+  })
+    
     res.json(result);
 }
 
