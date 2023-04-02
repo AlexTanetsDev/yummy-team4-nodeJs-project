8d56d0c9b8e0d6e12970183406dae73c3ea5c044
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2023-04-02 17:33:24 +0200

    Task 5(/recipes/:category)
    create getCategoriesRecepies
    add router get.(/:category)

--- routes/api/idRecipes.js
+++ routes/api/idRecipes.js
@@ -3,6 +3,15 @@ const ctrl = require("../../controllers/idRecipes");
 const { authentificate, isValidId } = require("../../middlewares");
 const router = express.Router();
 
+const ctrlCategory = require("../../controllers/getCategoriesRecepies");
+const controllersWrapper = require("../../helpers/controllersWrapper");
+
+router.get(
+  "/:category",
+  authentificate,
+  controllersWrapper(ctrlCategory.getCategoriesRecepies)
+);
+
 router.get("/:recipeId", authentificate, isValidId, ctrl.getById);
 
 module.exports = router;
