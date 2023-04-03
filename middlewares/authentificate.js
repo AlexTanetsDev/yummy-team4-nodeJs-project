e4b2f2e5789a189df76990ca077a2aaac9ba56af
SerhiiSamara <ndaair@gmail.com>
2023-04-03 19:31:32 +0300

    fix logout

--- middlewares/authentificate.js
+++ middlewares/authentificate.js
@@ -7,14 +7,14 @@ const { SECRET_KEY } = process.env;
 const authentificate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
-  
+
   if (bearer !== "Bearer") {
     next(HttpError(401));
   }
   try {
-    const { _id } = jwt.verify(token, SECRET_KEY);
-   
-    const user = await User.findById(_id);
+    const { id } = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(id);
+    console.log(user.token);
     if (!user || !user.token || user.token !== token) {
       next(HttpError(401));
     }
