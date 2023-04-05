0e04add6839c254d8130299dc2433f175ec689e6
IhorLyskov <99328897+IhorLyskov@users.noreply.github.com>
2023-04-05 11:30:26 +0300

    Update removeOwnRecipe.js

--- controllers/ownRecipes/removeOwnRecipe.js
+++ controllers/ownRecipes/removeOwnRecipe.js
@@ -1,24 +1,22 @@
 const { Recipe } = require("../../models/recipe");
 const { HttpError } = require("../../helpers");
 
-const removeOwnRecipe = async (req, res, next) => {
+const removeOwnRecipe = async (req, res) => {
   try {
     const { _id: userId } = req.user;
     const recipeId = req.query.recipeId;
 
-    const deletedRecipe = await Recipe.findById(
+    const deletedRecipe = await Recipe.findByIdAndDelete(
       { _id: recipeId },
       { author: userId }
     );
-
-    console.log(deletedRecipe);
 
     if (!deletedRecipe) {
       throw HttpError(404, "Not found");
     }
 
-    res.status(200).json({
-      data: { message: "Recipe deleted" },
+    res.json({
+      message: "Recipe deleted",
     });
   } catch (error) {
     throw HttpError(400, "Bad request");
