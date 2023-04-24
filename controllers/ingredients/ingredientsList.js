6180c3d3716cd43cf4894048e6a92cf010b43c23
IhorLyskov <99328897+IhorLyskov@users.noreply.github.com>
2023-04-24 23:50:39 +0300

    fix reverse
    and sorting of ingredients

--- controllers/ingredients/ingredientsList.js
+++ controllers/ingredients/ingredientsList.js
@@ -5,7 +5,7 @@ const { controllersWrapper } = require("../../helpers");
 const getAllIngredients = async (req, res) => {
   const result = await Ingredient.find();
 
-  res.json(result);
+  res.json(result.sort((a, b) => a.ttl.localeCompare(b.ttl)));
 };
 
 module.exports = {
