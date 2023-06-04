a65e0ad5a9f3aa7049ceca5ee72f151b11530e59
AlexTanetsDev <alexTanetsDev@gmail.com>
2023-06-04 12:54:53 +0300

    add test swagger to areas-list

--- routes/api/areaList/areaList.js
+++ routes/api/areaList/areaList.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const { authentificate } = require("../../../middlewares");
+const ctrl = require("../../../controllers/areaList");
 const router = express.Router();
 
-const ctrl = require("../../../controllers/areaList");
-const { authentificate } = require("../../../middlewares");
+/**
+ * @swagger
+ * /api/area-list:
+ *   get:
+ *     description: area-list
+ *     tags: [area-list]
+ *     responses:
+ *       200:
+ *         description: Success
+ */
 
 router.get("/", authentificate, ctrl.getAreaList);
 
