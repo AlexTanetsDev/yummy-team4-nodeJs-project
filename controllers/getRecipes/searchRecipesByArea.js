36f21057d3f27ae136eb5b2242c2219a02995ab5
AlexTanetsDev <alexTanetsDev@gmail.com>
2023-06-05 11:36:42 +0300

    add total in searchBy Area

--- controllers/getRecipes/searchRecipesByArea.js
+++ controllers/getRecipes/searchRecipesByArea.js
@@ -1,22 +1,19 @@
 const { Recipe } = require("../../models/recipe");
 
 const searchRecipesByArea = async (req, res) => {
-  // const { page = 1, limit = 4 } = req.query;
-  // const skip = (page - 1) * limit;
+  const { page = 1, limit = 12 } = req.query;
+  const skip = (page - 1) * limit;
 
   const { area } = req.params;
 
   const searchedRecipes = (
-    await Recipe.find(
-      { area },
-      "-createdAt -updatedAt"
-      // ,{
-      // skip,
-      // limit,
-      // }
-    )
+    await Recipe.find({ area }, "-createdAt -updatedAt", {
+      skip,
+      limit,
+    })
   ).map((recipe) => recipe.toObject());
-  res.json(searchedRecipes);
+  const total = searchedRecipes.length;
+  res.json({ searchedRecipes, total });
 };
 
 module.exports = searchRecipesByArea;
